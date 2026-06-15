713c6f5b7835d9cb4bceca7a530db260cc30314a
RickBm400 <rjbm29@gmail.com>
2026-06-15 00:08:57 -0500

    feat: task card with body

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,21 @@
 import Button from "./components/Button"
+import TaskCard from "./components/TaskCard.component";
 
 export function App() {
   return <div className="App w-screen h-screen grid place-items-center">
-      <div className="flex justify-between bg-white w-6xl p-8 rounded-lg shadow-lg">
+      <div className=" bg-white w-6xl p-8 rounded-lg shadow-lg">
+        <div className="flex justify-between items-center">
           <h2 className="text-lg">Incidencias y tareas</h2>
             <Button>
                    + Nueva Tarea
             </Button>
-      </div>    
+        </div>
+        <section id="task-list" className="w-full mt-8 space-y-4">
+            <TaskCard />
+            <TaskCard />
+            <TaskCard />
+        </section>
+      </div>
+
   </div>;
 }
--- apps/web/src/components/TaskCard.component.tsx
+++ apps/web/src/components/TaskCard.component.tsx
@@ -1,5 +1,9 @@
 export default function TaskCard() {
-    return <>
-        
-    </>
+    return <div className="bg-white p-4 rounded-lg shadow-md">
+        Revisar banners
+
+        <div className="w-full bg-gray-200 rounded border border-gray-300 mt-4 px-4 py-2">
+            <span className="text-gray-600 break-after-all">some text Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit beatae perspiciatis autem, maxime repellendus nostrum soluta omnis abaut inventore commodi? Animi, voluptates. Eos hic incidunt illum ad. Doloribus, omnis?</span>
+        </div>
+    </div>
 }
