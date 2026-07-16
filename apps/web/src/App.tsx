0d63c5b47453195d5c1d2bf84836a6609a8301db
RickBm400 <rjbm29@gmail.com>
2026-07-15 23:56:57 -0500

    feat: search bar components

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,27 @@
 import type { Task } from "@repo/task-manager-types";
 import Button from "./components/Button";
-import TaskCard from "./components/TaskCard.component";
+import TaskCard from "./components/TaskCard";
 import { TaskExamples } from "./constants/tasks";
+import { useEffect, useState } from "react";
 
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTasks([...TaskExamples]);
+  }, []);
+
   return (
     <div className="App w-screen h-screen grid place-items-center">
-      <div className=" bg-white w-6xl p-8 rounded-lg shadow-lg">
+      <div className=" bg-white w-6xl p-8 rounded-lg shadow-lg space-y-2">
+        <h2 className="text-lg font-bold">Incidencias y tareas</h2>
+
         <div className="flex justify-between items-center">
-          <h2 className="text-lg">Incidencias y tareas</h2>
           <Button>+ Nueva Tarea</Button>
         </div>
+
         <section id="task-list" className="w-full mt-8 space-y-4">
-          {TaskExamples.map((task: Task) => (
+          {tasks.map((task: Task) => (
             <TaskCard task={task} />
           ))}
         </section>
--- apps/web/src/components/SearchBar.tsx
+++ apps/web/src/components/SearchBar.tsx
@@ -3,3 +3,7 @@ import type React from "react";
 export function SearchBar(): React.ReactNode {
   return <></>;
 }
+
+export function FilterBox(): React.ReactNode {
+  return;
+}
