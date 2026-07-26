b65b4f7436dbace2fcf621e4164c226eb812a48c
RickBm400 <rjbm29@gmail.com>
2026-07-25 21:22:01 -0500

    feat components pattern structure

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,6 @@
 import { serve } from '@hono/node-server';
 import { swaggerUI } from '@hono/swagger-ui';
 import { Hono } from 'hono';
-import tasks from './routes/task.router.js';
 
 const app = new Hono();
 
@@ -12,40 +11,14 @@ const openApiDoc = {
     version: '1.0.0',
     description: 'API documentation for task management',
   },
-  paths: {
-    // Add your API paths here
-    '/health': {
-      get: {
-        description: 'Basic endpoint for health check',
-        responses: {
-          '200': {
-            description: 'OK',
-          },
-        },
-      },
-    },
-    // Add more endpoints as needed
-  },
+  paths: {},
 };
 
 app.get('/doc', (c) => c.json(openApiDoc));
 
-// Use the middleware to serve Swagger UI at /ui
 app.get('/ui', swaggerUI({ url: '/doc' }));
 
 app.get('/health', (c) => c.text('OK'));
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-});
-
-app.get('/greetins', (c) => {
-  c.status(201);
-  console.debug(c);
-  return c.json({ message: 'holamundo' });
-});
-
-app.route('/', tasks);
 
 serve(
   {
